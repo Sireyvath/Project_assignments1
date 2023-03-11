f6fdb26b32d556c56831100d504058996e608bcc
vitoutry <vitoutry@gmail.com>
2023-03-11 22:07:24 +0700

    fix image view path

--- src/main/java/com/example/project_assignment/HelloApplication.java
+++ src/main/java/com/example/project_assignment/HelloApplication.java
@@ -14,9 +14,9 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400, Color.BLUEVIOLET);
-        Image icon = new Image("D:\\School\\Term4\\Pratice OOP\\Project_assignment\\5164023.png");
+//        Image icon = new Image("D:\\School\\Term4\\Pratice OOP\\Project_assignment\\5164023.png");
 
-        stage.getIcons().add(icon);
+//        stage.getIcons().add(icon);
         stage.setTitle("Stock Management");
         stage.setScene(scene);
         stage.show();
