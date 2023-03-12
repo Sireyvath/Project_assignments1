e163b8194d221d272fa91c7b553fab2a09ea67c5
Sireyvath <sireyvath.teav@student.cadt.edu.kh>
2023-03-12 21:37:43 +0700

    Commit

--- src/main/java/com/example/project_assignment/HelloApplication.java
+++ src/main/java/com/example/project_assignment/HelloApplication.java
@@ -15,7 +15,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400, Color.BLUEVIOLET);
-        Image icon = new Image("D:\\School\\Term4\\Pratice OOP\\Project_assignment\\5164023.png");
+        Image icon = new Image("D:\\School\\Term4\\Pratice OOP\\Project_oop\\src\\main\\resources\\img\\5164023.png");
 
 
         stage.getIcons().add(icon);
